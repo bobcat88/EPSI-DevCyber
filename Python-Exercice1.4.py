d1070fb7f1c4364a18cfbc404ccb9a499bbd6d94
J.P <johanproust@gmail.com>
2024-04-11 18:17:54 +0200

    Finally fixed it

--- Python-Exercice1.4.py
+++ Python-Exercice1.4.py
@@ -1,5 +1,7 @@
 import datetime as dt
 from datetime import datetime as d
+import tkinter as tk
+from tkinter import filedialog as fd
 import json
 
 personnes = []
@@ -7,7 +9,7 @@ personnes = []
 def age_reel(personne):
     today = d.today()
     anniv_input = d.strptime(personne["date d'anniversaire"],"%Y/%m/%d")
-    age = today.year - anniv_input.year - ((today.month, today.day))<(anniv_input.month,anniv_input.day)
+    age = today.year - anniv_input.year - ((today.month, today.day) < (anniv_input.month, anniv_input.day))
     return age
     
 while True:
@@ -16,7 +18,9 @@ while True:
     personne["prénom"] = input("Prénom: ")
     personne["date d'anniversaire"] = input("date d'anniversaire au format AAAA/MM/JJ: ")
     personne["ville"] = input("Ville: ")
-    personne["age"] = age_reel()
+    personne["age"] = age_reel(personne)
+    print(f"Age: {personne['age']}")    
+    
     personnes.append(personne)
     choice = input("Add another person? (y/n) ")
     if choice != "y":
@@ -24,6 +28,16 @@ while True:
 
 print(personnes)
 
-with open('ExerciceData.json', 'w', encoding='utf-8') as f:
-    json.dump(personnes, f, ensure_ascii=False, indent=4)
+def save_to_json(data):
+    tk.Tk().withdraw()
+    folder_path = fd.askdirectory()
+    if folder_path:
+        filename = fd.asksaveasfilename(initialdir=folder_path, defaultextension=".json", filetypes=[("JSON files", "*.json")])
+        if filename:
+            with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
+            print("Data saved to:", filename)
+
+save_to_json(personnes)
+
 print("Données enregistrées")
